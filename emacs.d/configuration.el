34097ea466cd1f31e1958ed304817841bc172c63
Deyan Dyankov <deyan.dyankov@distributed-systems.eu>
2018-09-19 00:37:52 +0100

    commit

--- emacs.d/configuration.el
+++ emacs.d/configuration.el
@@ -3,8 +3,36 @@
 (sensible-defaults/use-all-keybindings)
 (sensible-defaults/backup-to-temp-directory)
 
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-nord t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme (all-the-icons must be installed!)
+;; (doom-themes-neotree-config)
+;; or for treemacs users
+;; (doom-themes-treemacs-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
 (require 'org)
 (setq org-agenda-files '("~/org"))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-export-coding-system 'utf-8)
+;;(setq org-ellipsis "⤵")
+(define-key global-map "\C-ca" 'org-agenda)
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(add-hook 'calendar-load-hook (lambda () (calendar-set-date-style 'european)))
+(setq calendar-week-start-day 1)
